46ca5cca8d325863f99547530e9e089b5f91bc1c
prasanthipenmadu <prasanthipl@gmail.com>
2019-06-23 22:32:20 -0400

    All tests passed.

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -1,8 +1,51 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by leon on 1/29/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+    Integer[] intArray;
+
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+        this.intArray=intArray;
+    }
+
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        List<Integer> al=new ArrayList<>();
+        int count=0;
+        for(Integer i:intArray){
+            count = 0;
+            for(Integer j:intArray){
+                if(i==j)
+                    count++;
+            }
+            if(count<maxNumberOfDuplications){
+                al.add(i);
+            }}Integer [] array11=  al.toArray(new Integer[al.size()]);
+        return array11;
+    }
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        List<Integer> al=new ArrayList<>();
+        int count=0;
+        for(Integer i:intArray){
+            count = 0;
+            for(Integer j:intArray){
+                if(i==j)
+                    count++;
+            }
+            if(count!=exactNumberOfDuplications){
+                    al.add(i);
+            }}
+        Integer [] array1=  al.toArray(new Integer[al.size()]);
+        return array1;
+
+    }
 }
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -1,8 +1,51 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by leon on 1/28/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
+    String[] intArray;
+    public StringDuplicateDeleter(String[] intArray) {
+        super(intArray);
+        this.intArray=intArray;
+    }
+
+    @Override
+    public String[] removeDuplicates(int maxNumberOfDuplications) {
+        List<String> al=new ArrayList<>();
+        int count=0;
+        for(String i:intArray){
+            count = 0;
+            for(String j:intArray){
+                if(i==j)
+                    count++;
+            }
+            if(count<maxNumberOfDuplications){
+                al.add(i);
+            }}String [] array11=  al.toArray(new String[al.size()]);
+        return array11;
+    }
+
+    @Override
+    public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        List<String> al=new ArrayList<>();
+        int count=0;
+        for(String i:intArray){
+            count = 0;
+            for(String j:intArray){
+                if(i==j)
+                    count++;
+            }
+            if(count!=exactNumberOfDuplications){
+                al.add(i);
+            }}
+        String [] array1=  al.toArray(new String[al.size()]);
+        return array1;
+
+    }
+
 }
